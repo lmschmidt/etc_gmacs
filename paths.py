916889d22ea319b203afa9de4c577b93302cb85a
Luke Schmidt <lmschmidt@tamu.edu>
2019-01-04 13:35:24 -0600

    more descriptive galaxy names

--- paths.py
+++ paths.py
@@ -16,6 +16,9 @@ ccd_path = [parentDir / 'core' / 'e2v-astro-multi-2-DD.txt',  parentDir / 'core'
 vega_file = parentDir / 'core' / 'alpha_lyr_stis_005.ascii'
 mirror_file = parentDir / 'core/mirror_300-1200nm.txt'
 galaxy_files = ['SB1', 'SB2', 'SB3', 'SB4', 'SB5', 'SB6', 'S0', 'Sa', 'Sb', 'Sc', 'bulge', 'ellipticals', 'lbg_all_flam']
+galaxy_labels = ['Starbursts with E(B-V) < 0.10', 'Starbursts with  0.11 < E(B-V) < 0.21', 'Starbursts with  0.25 < E(B-V) < 0.35',
+                 'Starbursts with  0.39 < E(B-V) < 0.50', 'Starbursts with  0.51 < E(B-V) < 0.60', 'Starbursts with  0.61 < E(B-V) < 0.70',
+                 'S0 Galaxies', 'Sa Galaxies', 'Sb Galaxies ', 'Sc Galaxies', 'Galactic Bulges', 'Elliptical Galaxies', 'Lyman-break Galaxies']
 stellar_files = ['o5v.dat', 'b0v.dat', 'b57v.dat', 'a0v.dat', 'a5v.dat', 'f0v.dat', 'f5v.dat', 'g0v.dat', 'g5v.dat', 'k0v.dat', 'k5v.dat', 'm0v.dat', 'm5v.dat']
 skyfiles = ['00d_315-1200nm.csv', '03d_315-1200nm.csv', '07d_315-1200nm.csv', '10d_315-1200nm.csv', '14d_315-1200nm.csv']
 filter_files = ['BesU.dat', 'BesB.dat', 'BesV.dat', 'BesR.dat', 'BesI.dat', 'u.dat', 'g.dat', 'r.dat', 'i.dat', 'z.dat']
