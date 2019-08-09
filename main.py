5d05d27671dec0fd81a440107825519c9cc84466
Luke Schmidt <lschmidt@physics.tamu.edu>
2019-08-09 12:22:31 -0500

    import pandas in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from os.path import dirname,  join
 
 # import numpy as np
+import pandas as pd
 
 from bokeh.plotting import figure
 from bokeh.layouts import widgetbox, column, layout, gridplot  # , row
